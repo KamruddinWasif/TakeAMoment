d5277d460ec784f7a137f8af6ee76bf26d3b0fa5
Wasif Kamruddin <kamruddinwasif@gmail.com>
2023-06-05 23:33:38 +1000

    Initial Commit

---
+++ TakeAMoment/TakeAMomentApp.swift
@@ -0,0 +1,17 @@
+//
+//  TakeAMomentApp.swift
+//  TakeAMoment
+//
+//  Created by Wasif Kamruddin on 6/5/23.
+//
+
+import SwiftUI
+
+@main
+struct TakeAMomentApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
